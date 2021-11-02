588538cfafe0f6b61422e3d056dfa85da288dbc8
Daniel Brannon <danielbrannon@Daniels-MacBook-Air.local>
2021-11-02 15:11:22 -0400

    added some MVVN

---
+++ RestaurantProject/RestaurantProject/View/MainCollectionViewCell/myCell.swift
@@ -0,0 +1,17 @@
+//
+//  myCell.swift
+//  RestaurantProject
+//
+//  Created by Daniel Brannon on 11/2/21.
+//
+
+import UIKit
+
+class myCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ RestaurantProject/RestaurantProject/View/MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MainViewController.swift
+//  GroupProject
+//
+//  Created by Daniel Brannon on 11/2/21.
+//
+import UIKit
+
+class MainViewController: UIViewController {
+    let collectionView = UICollectionView()
+    var myData: [Restaurants] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NetworkingRequest().fetch() { response in
+            DispatchQueue.main.async {
+                self.myData = response.restaurants
+                self.collectionView.reloadData()
+                self.title = "Lunch Tyme"
+                let nib = UINib(nibName: "myCell", bundle: nil)
+                self.collectionView.register(nib, forCellWithReuseIdentifier: "myCell")
+            }
+       
+    }
+        func setupCollectionView() {
+               
+    }
+}
+}
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+extension MainViewController: UICollectionViewDelegateFlowLayout {}
