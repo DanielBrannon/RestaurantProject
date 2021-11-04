eefcec1d24e2aecd74b2a35f353bd0f423af3a9d
Daniel Brannon <daniel-brannon@hotmail.com>
2021-11-04 14:13:08 -0400

    finished mapview

--- RestaurantProject/RestaurantProject/View/MapViewController.swift
+++ RestaurantProject/RestaurantProject/View/MapViewController.swift
@@ -19,12 +19,71 @@ class MapVC: UIViewController, UIPageViewControllerDelegate {
     @IBOutlet weak var phone: UILabel!
     @IBOutlet weak var socialMedia: UILabel!
     
-    var delegate: MainViewControllerDelegate? = Coordinator()
+    var restaurantDetails: Restaurants?
     var myUrl: String?
+    var myStorageVariable: String = ""
+    var myAddressVariable: String = ""
+    var centerPoint = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        if let longitude = restaurantDetails?.location?.lng {
+            if let latitude = restaurantDetails?.location?.lat {
+                centerPoint.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                mapView.addAnnotation(centerPoint)
+                
+            }
+        }
+        mapView.centerCoordinate = CLLocationCoordinate2D(latitude: restaurantDetails!.location!.lat, longitude: restaurantDetails!.location!.lng)
+        mapView.region.span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+        centerPoint.title = restaurantDetails?.name
+        restaurantName.text = restaurantDetails?.name
+        restaurantType.text = restaurantDetails?.category
+        streetName.text = restaurantDetails?.location?.address
+        if let city = restaurantDetails?.location?.city {
+            myAddressVariable += city
+        }
+        if let state = restaurantDetails?.location?.state {
+            myAddressVariable +=  ", " + state
+        }
+        if let zipCode = restaurantDetails?.location?.postalCode {
+            myAddressVariable += " "  + zipCode
+        }
+        address2.text = myAddressVariable
+        phone.text = restaurantDetails?.contact?.formattedPhone
+        if let facebook = restaurantDetails?.contact?.facebook {
+            if myStorageVariable == "" {
+            myStorageVariable += facebook
+            } else {
+            myStorageVariable += myStorageVariable + "\n @" + facebook
+            }
+        }
+        if let fbookName = restaurantDetails?.contact?.facebookName {
+            if myStorageVariable == "" {
+                myStorageVariable += fbookName
+            } else {
+                myStorageVariable += myStorageVariable + "\n @" + fbookName
+            }
+        }
+        if let fbookUsername = restaurantDetails?.contact?.facebookUsername {
+            if myStorageVariable == "" {
+                myStorageVariable += fbookUsername
+            } else {
+                myStorageVariable += myStorageVariable + "\n @" + fbookUsername
+            }
+        }
+        if let twitter = restaurantDetails?.contact?.twitter {
+            if myStorageVariable == "" {
+                myStorageVariable += twitter
+            } else {
+                myStorageVariable += myStorageVariable + "\n @" + twitter
+            }
+        }
+        if myStorageVariable == "" {
+            socialMedia.text = ""
+        } else {
+            socialMedia.text = "@" + myStorageVariable
+        }
     }
+    
 }
